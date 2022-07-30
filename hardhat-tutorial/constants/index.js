a18c6906d65f6245688b8e985dae01c16579662e
angelArias <angel@angelarias.es>
2022-07-30 18:09:15 +0200

    subo proyecto ico

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,5 @@
+
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xc25bd7726230d121fa19820c5a30cff260507946";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
